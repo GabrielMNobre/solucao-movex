7154f5bccd12e5690711eb27bfb240e8e088b902
Dasilva-96 <64237084+Dasilva-96@users.noreply.github.com>
2020-11-27 00:13:36 -0200

    Adc. JCalander tela relatório

---
+++ solucaoMovex/src/main/java/Model/Relatorio.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Danga
+ */
+public class Relatorio {
+    
+    private Date dataInicial;
+    private Date dataFinal;
+    private int numeroPedido;
+    private String codVendedor;
+    private String codCliente;
+    private double valorTotal;
+
+    public Date getDataInicial() {
+        return dataInicial;
+    }
+
+    public void setDataInicial(Date dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+
+    public Date getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(Date dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+    public int getNumeroPedido() {
+        return numeroPedido;
+    }
+
+    public void setNumeroPedido(int numeroPedido) {
+        this.numeroPedido = numeroPedido;
+    }
+
+    public String getCodVendedor() {
+        return codVendedor;
+    }
+
+    public void setCodVendedor(String codVendedor) {
+        this.codVendedor = codVendedor;
+    }
+
+    public String getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(String codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+    
+    
+}
